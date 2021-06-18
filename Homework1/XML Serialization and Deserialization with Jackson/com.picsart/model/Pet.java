2fe8a62f5c188290db34aca178232149b56dbd0c
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-06-18 14:35:56 +0400

    Create Pet.java

---
+++ Homework1/XML Serialization and Deserialization with Jackson/com.picsart/model/Pet.java
@@ -0,0 +1,28 @@
+package com.picsart.model;
+
+public class Pet {
+    private String type;
+
+    public Pet() {
+
+    }
+
+    public Pet(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "type='" + type + '\'' +
+                '}';
+    }
+}
