8ffae0396fd1c0108118ef3dc1aeaa2bb8c1be3f
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-06-18 14:45:39 +0400

    Create TimerService.java

---
+++ Homework3/Timer(Minutes:Seconds)/com.picsart/service/TimerService.java
@@ -0,0 +1,18 @@
+package com.picsart.service;
+
+import com.picsart.model.MyRunnable;
+
+import java.util.Scanner;
+
+public class TimerService {
+    public static void createTimer() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter minutes: ");
+        int currentMin = scanner.nextInt();
+        System.out.println("Enter seconds: ");
+        int currentSec = scanner.nextInt();
+        Thread t = new Thread(new MyRunnable(currentMin, currentSec));
+        System.out.println("Timer:");
+        t.start();
+    }
+}
