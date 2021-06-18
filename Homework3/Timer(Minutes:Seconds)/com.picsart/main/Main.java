6a26525651d0247a455c7e8f2ce084906cf4a8ee
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-06-18 14:44:39 +0400

    Create Main.java

---
+++ Homework3/Timer(Minutes:Seconds)/com.picsart/main/Main.java
@@ -0,0 +1,9 @@
+package com.picsart.main;
+
+import com.picsart.service.TimerService;
+
+public class Main {
+    public static void main(String[] args) {
+        TimerService.createTimer();
+    }
+}
