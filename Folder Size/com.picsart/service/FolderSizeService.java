0374fa63cb80f1f4753cdf63bc82e133deb288dc
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-06-21 11:07:34 +0400

    Create FolderSizeService.java

---
+++ Folder Size/com.picsart/service/FolderSizeService.java
@@ -0,0 +1,30 @@
+package com.picsart.service;
+
+import java.io.File;
+
+public class FolderSizeService {
+    public static long getFolderSize(File folder) {
+        long length = 0;
+
+        File[] files = folder.listFiles();
+
+        int count = files.length;
+
+        for (int i = 0; i < count; i++) {
+            if (files[i].isFile()) {
+                length += files[i].length();
+            }
+            else {
+                length += getFolderSize(files[i]);
+            }
+            if(i != count - 1)
+                System.out.println(length/1024 + "Kb");
+            try {
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return length;
+    }
+}
