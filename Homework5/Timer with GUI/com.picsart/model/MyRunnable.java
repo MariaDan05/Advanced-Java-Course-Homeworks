3556c5ca4c64724d9b441f3e186cefcccb27c8a1
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-06-24 10:28:47 +0400

    Create MyRunnable.java

---
+++ Homework5/Timer with GUI/com.picsart/model/MyRunnable.java
@@ -0,0 +1,47 @@
+package com.picsart.model;
+
+import com.picsart.service.InvalidInputException;
+import com.picsart.service.PrintValidator;
+
+import javax.swing.*;
+
+public class MyRunnable implements Runnable{
+    private int currentMin;
+    private int currentSec;
+    private JTextArea output;
+
+    public MyRunnable(int currentMin, int currentSec, JTextArea output) {
+        if(currentMin >= 0 && currentMin <= 59) {
+            this.currentMin = currentMin;
+        } else {
+            throw new InvalidInputException("Invalid Minutes, try again");
+        }
+        if(currentSec >= 0 && currentSec <= 59) {
+            this.currentSec = currentSec;
+        } else {
+            throw new InvalidInputException("Invalid Seconds, try again");
+        }
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+        while (currentMin >= 0 && currentMin <= 59 && currentSec >= 0 && currentSec <= 59) {
+            String str1 = PrintValidator.minutesStyle(currentMin);
+            String str2 = PrintValidator.secondsStyle(currentSec);
+            if(currentSec-- == 0){
+                currentSec = 59;
+                currentMin--;
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            output.setText(str1 + str2);
+            output.setEditable(false);
+        }
+    }
+
+}
+
