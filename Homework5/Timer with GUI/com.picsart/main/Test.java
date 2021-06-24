cd4e51243da51bca2e461f3e85c6bc65b36f3103
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-06-24 10:28:05 +0400

    Create Test.java

---
+++ Homework5/Timer with GUI/com.picsart/main/Test.java
@@ -0,0 +1,11 @@
+package com.picsart.main;
+
+import com.picsart.model.MyTimer;
+
+public class Test
+{
+    public static void main(String[] args) {
+        MyTimer myTimer = new MyTimer();
+        myTimer.setVisible(true);
+    }
+}
